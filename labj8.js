5f2fa66262dd9101c133838b13c2299c786263ef
breath-in-deep-a-day-at-a-time <vharshu08@gmail.com>
2024-05-08 14:05:16 +0530

    created third commit

---
+++ labj8.js
@@ -0,0 +1,9 @@
+function sortStringAlphabetically(str) {
+    // Convert the string to an array of characters, sort it alphabetically, and then join it back into a string
+    return str.split('').sort().join('');
+    }
+    // Example usage:
+    const inputString = "hello world";
+    const sortedString = sortStringAlphabetically(inputString);
+    console.log("Original string:", inputString);
+    console.log("Sorted string:", sortedString);
